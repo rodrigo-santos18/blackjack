4166ed0d3465e81e279d4849f248b2a869476358
Rodrigo dos Santos <rodrigo.f.santos@tecnico.ulisboa.pt>
2023-03-19 18:59:55 +0000

    Versão com pot, player, dealer e card como classes. Zero testes

--- blackjack.py
+++ blackjack.py
@@ -1,17 +1,44 @@
 # Projeto BLACKJACK
 
 import random
+import winsound
 
-class Cards:
-    def __init__(self):
+class Card:
+   
+    def __init__(self, int):
+        self._id = int
+        
+        if self._id / 13 <= 1:
+            self._suit = "clubs"
+            self._idFactor = 0 
+        
+        elif 1 < self._id / 13 <= 2:
+            self._suit = "diamonds"
+            self._idFactor = 1
+        
+        elif 2 < self._id / 13 <= 3:
+            self._suit = "hearts"
+            self._idFactor = 2  
+        
+        else:
+            self._suit = "spades"
+            self._idFactor = 3
+
+        self._value = self._id - self._idFactor * 13
+
+    def suit():
+        return Card._suit
+    
+    def value():
+        return Card._value
         
 
-class Casino:
 
+class Casino:
     def __init__(self):
-        _players = []
-        _deck = []
-        _shops = []
+        self._players = []
+        self._deck = []
+        self._shops = []
 
     def __iadd__(player):
         if player not in Casino.players:
@@ -43,7 +70,3 @@ class Player:
     def __init__(self):
         hand = []
         tricks = []
-
-    def get_card():
-        
-
