cd9d5158cca67c81e78e117cadfaaffb2a80b0de
Rodrigo dos Santos <rodrigo.f.santos@tecnico.ulisboa.pt>
2023-03-19 19:07:45 +0000

    Commit para fazer o setup do credential helper do terminal (nada mudou desde o último commit)

--- blackjack.py
+++ blackjack.py
@@ -1,10 +1,10 @@
 # Projeto BLACKJACK
 
 import random
-import winsound
 
 class Card:
-   
+
+
     def __init__(self, int):
         self._id = int
         
@@ -32,41 +32,70 @@ class Card:
     def value():
         return Card._value
         
-
-
-class Casino:
-    def __init__(self):
-        self._players = []
-        self._deck = []
-        self._shops = []
-
-    def __iadd__(player):
-        if player not in Casino.players:
-            Casino.players.append(player)
-        else:
-            print("This player is already in the Casino!")
-
-    def __isub__(player):
-        if player in Casino.players:
-            Casino.players.remove(player)
-        else:
-            print("This player is not in the Casino!")
-
-    def players():
-        return Casino.players
-
-    def deck():
-        return Casino.deck
-
-    def distribute(card):
-        for i, gambler in enumerate(Casino.players()):
-           gambler += random.choice(Casino.deck)
-
-        
-
+class Pack:
+    
 
 class Player:
 
+    def __init__(self,cash):
+        self._hand = []
+        self._tricks = []
+        if isinstance(cash, float):
+            self._chips = cash // 50
+        else: 
+            print("That's not cash... come on, the House is not stupid.")
+
+    def bet(n_chips):
+        Player._chips -= n_chips
+        Pot += n_chips
+    
+    def hand():
+        return self._hand
+
+    def ask_Card():
+        hand_value = 0
+        for indx, card in enumerate(Player.hand()):
+            card.value += hand_value
+
+        
+
+class Dealer(Player):
+
+
     def __init__(self):
-        hand = []
-        tricks = []
+        self._pack = []
+        self._betPot = 0
+
+    def __iadd__(bet):
+        Dealer._betPot += bet 
+
+    def __isub__(bet):
+        Dealer._betPot -= bet 
+
+    def players():
+        return Dealer.players
+
+    def pack():
+        return Dealer.pack
+
+             
+
+
+class Pot:
+
+
+    def __init__(self):
+        self._pot = 0.0
+
+    def __iadd__(bet):
+        Pot._pot += bet
+
+    def __isub__(bet)_
+        Pot._pot -= bet
+
+    def reset():
+        Pot._pot = 0.0
+
+
+
+        
