22fa79408c0974a10d7e7ca67c40de8359454f07
Rodrigo dos Santos <rodrigo.f.santos@tecnico.ulisboa.pt>
2023-03-19 20:13:22 +0000

    Commit com a versão 'stripped down' dos objetos do jogo

--- blackjack.py
+++ blackjack.py
@@ -1,12 +1,13 @@
-# Projeto BLACKJACK
+# Objetos do Blackjack (cartas, jogador e pack - o conjunto de cartas, o deck)
+
 
 import random
 
+
 class Card:
 
-
-    def __init__(self, int):
-        self._id = int
+    def __init__(self, id):
+        self._id = id
         
         if self._id / 13 <= 1:
             self._suit = "clubs"
@@ -25,20 +26,42 @@ class Card:
             self._idFactor = 3
 
         self._value = self._id - self._idFactor * 13
-
+        
     def suit():
         return Card._suit
     
     def value():
         return Card._value
-        
-class Pack:
     
+    def id():
+        return Card._id
+
+
+class Pack:
+
+    def __init__(self) -> None:
+        self._pack = set()
+        count = 0
+        while count < 7:
+            for i in range(1, 53):
+                self._pack.add(Card(i))
+            count += 1
+
+    def packIds():
+        packIds = []
+        for card in Pack._pack:
+            packIds.append(card.id())
+        return packIds
+    
+    def pack():
+        return Pack._pack
+            
 
 class Player:
 
     def __init__(self,cash):
         self._hand = []
+        self._handValue = sum([card.Value for card in self._hand])
         self._tricks = []
         if isinstance(cash, float):
             self._chips = cash // 50
@@ -50,52 +73,18 @@ class Player:
         Pot += n_chips
     
     def hand():
-        return self._hand
+        return Player._hand
+    
+    def hand_reset():
+        Player._hand = []
 
-    def ask_Card():
-        hand_value = 0
-        for indx, card in enumerate(Player.hand()):
-            card.value += hand_value
+    def hand_value():
+        return Player._handValue
 
-        
-
-class Dealer(Player):
-
-
-    def __init__(self):
-        self._pack = []
-        self._betPot = 0
-
-    def __iadd__(bet):
-        Dealer._betPot += bet 
-
-    def __isub__(bet):
-        Dealer._betPot -= bet 
-
-    def players():
-        return Dealer.players
-
-    def pack():
-        return Dealer.pack
-
-             
-
-
-class Pot:
-
-
-    def __init__(self):
-        self._pot = 0.0
-
-    def __iadd__(bet):
-        Pot._pot += bet
-
-    def __isub__(bet)_
-        Pot._pot -= bet
-
-    def reset():
-        Pot._pot = 0.0
-
-
-
-        
+    def ask_card():
+        id = random.choice(Pack.packIds())
+        for card in Pack.pack():
+            if card.id() == id:
+                Pack.remove(card)
+                Player._Hand.append(card)
+                break
